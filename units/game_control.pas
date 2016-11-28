675bd2994d3c45a3deb869ffa8cb13c0cd747a0c
cpicanco <cpicanco42@gmail.com>
2016-11-27 22:48:55 -0300

    update headers

--- units/game_control.pas
+++ units/game_control.pas
@@ -1,3 +1,12 @@
+{
+  Stimulus Control
+  Copyright (C) 2014-2016 Carlos Rafael Fernandes Picanço, Universidade Federal do Pará.
+
+  The present file is distributed under the terms of the GNU General Public License (GPL v3.0).
+
+  You should have received a copy of the GNU General Public License
+  along with this program. If not, see <http://www.gnu.org/licenses/>.
+}
 unit game_control;
 
 {$mode objfpc}{$H+}
@@ -378,6 +387,8 @@ begin
   FRowBase:=AValue;
 end;
 
+// many thanks to howardpc for this:
+// http://forum.lazarus.freepascal.org/index.php/topic,34559.msg227585.html#msg227585
 function TGameControl.AskQuestion(AQuestion: string): UTF8string;
 var
   Prompt: TForm;
