e9ebfa8fd3d1a7238ad8653d2567e772d55ed59b
Rafael Picanço <cpicanco@users.noreply.github.com>
2017-08-30 22:48:29 -0300

    Update entropy_analysis.py

--- data_analysis_helpers/entropy_analysis.py
+++ data_analysis_helpers/entropy_analysis.py
@@ -1,8 +1,19 @@
 # -*- coding: utf-8 -*-
 """
+Free-mtrix - Free cultural selection and social behavior experiments.
+Copyright (C) 2016-2017 Carlos Rafael Fernandes Picanço, Universidade Federal do Pará.
+
+The present file is distributed under the terms of the GNU General Public License (GPL v3.0).
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see http://www.gnu.org/licenses/.
+"""
+
+"""
 Lê colunas de interesse e calcula a porcentagem móvel
 de acordo com o valor de data_interval
 """
+
 from math import log
 # import numpy as np
 
