bd70cbe9b8da38ebef4e0ac2eff43cfc98fdc0e7
Rafael Picanço <cpicanco@users.noreply.github.com>
2017-08-30 22:49:48 -0300

    add missing headers (#13)
    * Update variability_analysis.py
    * Update entropy_analysis.py

--- data_analysis_helpers/variability_analysis.py
+++ data_analysis_helpers/variability_analysis.py
@@ -1,4 +1,13 @@
 # -*- coding: utf-8 -*-
+"""
+Free-mtrix - Free cultural selection and social behavior experiments.
+Copyright (C) 2016-2017 Carlos Rafael Fernandes Picanço, Universidade Federal do Pará.
+
+The present file is distributed under the terms of the GNU General Public License (GPL v3.0).
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see http://www.gnu.org/licenses/.
+"""
 import os
 from math import factorial as f
 
