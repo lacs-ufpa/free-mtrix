a8439ef3334720feebab033a5aebb271b4567462
Rafael Picanço <cpicanco42@gmail.com>
2019-01-27 20:41:44 -0300

    use release version to run-time tests

--- experiment_runner/run_participants.sh
+++ experiment_runner/run_participants.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-$PWD/Participant1/experiment_runner_linux_64bits_debug --player &
+$PWD/Participant1/experiment_runner_linux_64bits --player &
 sleep 2
-$PWD/Participant2/experiment_runner_linux_64bits_debug --player &
+$PWD/Participant2/experiment_runner_linux_64bits --player &
 sleep 2
-$PWD/Participant3/experiment_runner_linux_64bits_debug --player &
+$PWD/Participant3/experiment_runner_linux_64bits --player &
