1b209f00560248c1c4316ce4f32102ea86b95ad2
Rafael Picanço <cpicanco@users.noreply.github.com>
2019-09-19 08:50:50 -0300

    Update game_experiment.pas

--- experiment_runner/units/game_experiment.pas
+++ experiment_runner/units/game_experiment.pas
@@ -1,12 +1,14 @@
-{
-  Free-mtrix - Free cultural selection and social behavior experiments.
-  Copyright (C) 2016-2017 Carlos Rafael Fernandes Picanço, Universidade Federal do Pará.
+/*   
+  Free-mtrix - Free cultural selection and social behavior experiments.   
+  Copyright (C) 2016-2019 Carlos Rafael Fernandes Picanço.   
+  Copyright (C) 2016-2019 Thais Maria Monteiro Guimarães.   
+  Copyright (C) 2016-2019 Universidade Federal do Pará.
 
   The present file is distributed under the terms of the GNU General Public License (GPL v3.0).
 
-  You should have received a copy of the GNU General Public License
-  along with this program. If not, see <http://www.gnu.org/licenses/>.
-}
+  You should have received a copy of the GNU General Public License   
+  along with this program. If not, see <http://www.gnu.org/licenses/>.   
+*/
 unit game_experiment;
 
 {$mode objfpc}{$H+}
